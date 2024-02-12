81972590f2f85b91bf820f42684b09b9c53eb07d
Ulrike Uhlig <u@451f.org>
2024-02-12 15:59:14 +0100

    improve a bit

--- documentation/assets/js/plotly-leadership.js
+++ documentation/assets/js/plotly-leadership.js
@@ -1,5 +1,4 @@
-//https://codepen.io/u451f/pen/vYPzWQY example data
-d3.csv("../data/leadership/leadership_dnsop_test.csv", function(err, rows){
+d3.csv("../data/leadership/leadership_dnsop.csv", function(err, rows){
     // console.log("Rows", rows);
 
     function unpack(rows, key) {
@@ -16,21 +15,38 @@ d3.csv("../data/leadership/leadership_dnsop_test.csv", function(err, rows){
 
     var traces = [];
     var hexcolor;
-    // TODO: do some kind of grouping of affiliations first
+
+    // TODO: test on some other files and if needed
+    // do some kind of grouping of affiliations first
+
     for(var i = 0; i < rows.length; i++) {
         hexcolor = getColorCode();
-        traces.push({
-            type: "scatter",
-            mode: 'lines',
-            line: {
-                color: hexcolor,
-                width: 25
-            },
-            name: rows[i]['affiliation'],
-            text: rows[i]['name'],
-            x: [ rows[i]['datetime_min'], rows[i]['datetime_max'] ],
-            y: [ rows[i]['affiliation'], rows[i]['affiliation'] ],
-        });
+        /* This bit should probably be done when creating the CSV*/
+        var datetime_min, datetime_max;
+        if(!rows[i]['datetime_min']) {
+           datetime_min = "2007-01-01 00:00:00+00:00";
+        } else {
+            datetime_min = rows[i]['datetime_min'];
+        }
+        if(!rows[i]['datetime_max']) {
+           datetime_max = "2024-01-01 00:00:00+00:00";
+        } else {
+            datetime_max = rows[i]['datetime_max'];
+        }
+        if(datetime_min != datetime_max) { // should I be doing this or is this a pre-cleanup?
+            traces.push({
+                type: "scatter",
+                mode: 'lines',
+                line: {
+                    color: hexcolor,
+                    width: 25
+                },
+                name: rows[i]['affiliation'],
+                text: rows[i]['name'],
+                x: [ datetime_min, datetime_max ],
+                y: [ rows[i]['affiliation'], rows[i]['affiliation'] ],
+            });
+        }
     }
 
     console.log("traces", traces);
@@ -38,11 +54,16 @@ d3.csv("../data/leadership/leadership_dnsop_test.csv", function(err, rows){
     var data = traces;
 
     var layout = {
-        title: 'WG Leadership / chairs',
+        title: 'WG Leadership',
         xaxis: {
             title: 'Chairing period',
-            //tickformat: '%Y'
+            tickformat: '%Y'
         },
+        yaxis: {
+            automargin: true,
+            visible: false,
+            showgrid: false
+        }
     };
 
     Plotly.newPlot('myDiv', data, layout);
