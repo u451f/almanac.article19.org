ffa2696f386abd85b2f4faf9bb956d7cfa632547
Ulrike Uhlig <u@451f.org>
2024-02-12 16:47:14 +0100

    add transparency

--- documentation/assets/js/get-color-code.js
+++ documentation/assets/js/get-color-code.js
@@ -1,22 +1,25 @@
 /*
  * Return a color code for matched affiliations
  * or generate a random color code for unmatched ones
- *
+ * Format: hexadecimal with transparency value (90)
  * */
 function getColorCode(affiliation) {
     var hexcolor;
     switch (affiliation) {
         case 'Google':
-            hexcolor = "#ffcc00";
+            hexcolor = "#ffcc0090";
+        break;
+        case 'mit.edu':
+            hexcolor = "#ffcc0090";
         break;
         case 'cisco.com':
-            hexcolor = "#ffcc00";
+            hexcolor = "#ffcc0090";
         break;
         case 'DENIC':
-            hexcolor = "#ffcc00";
+            hexcolor = "#ffcc0090";
         break;
         default:
-            hexcolor = '#' + Math.floor(Math.random() * 16777215).toString(16)
+            hexcolor = '#' + Math.floor(Math.random() * 16777215).toString(16) + "90"
     }
     return hexcolor;
 }
