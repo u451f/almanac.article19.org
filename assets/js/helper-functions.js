7aa94d0af7a5c30d26355e1f9dd97eeba43b09cd
Ulrike Uhlig <u@451f.org>
2024-02-15 16:26:36 +0100

    add JS for viz

---
+++ assets/js/helper-functions.js
@@ -0,0 +1,38 @@
+function isNullOrUndefined(value) {
+    return value === undefined || value === null;
+}
+
+function unpack(rows, key) {
+    return rows.map(function(row) {
+         return row[key];
+     });
+}
+
+/*
+ * Return a color code for matched affiliations
+ * or generate a random color code for unmatched ones
+ * Format: hexadecimal with transparency value (90)
+ * */
+function getColorCode(affiliation) {
+    var hexcolor;
+    //console.log("affiliation", affiliation);
+    switch (affiliation) {
+        case 'Google':
+            hexcolor = "#ffcc0090";
+        break;
+        case 'mit.edu':
+            hexcolor = "#ffcc0090";
+        break;
+        case 'Huawei':
+            hexcolor = "#ffcc0090";
+        break;
+        case 'DENIC':
+            hexcolor = "#ffcc0090";
+        break;
+        default:
+            hexcolor = '#' + Math.floor(Math.random() * 16777215).toString(16) + "90"
+    }
+    return hexcolor;
+}
+
+export { getColorCode, isNullOrUndefined, unpack };
