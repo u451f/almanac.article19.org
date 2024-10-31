19b54dc013390b6b4edd265c685c7b2d72cdd48f
Ulrike Uhlig <u@451f.org>
2024-10-31 14:12:55 +0100

    Plotly JS for dashboard plots, adapted as modules

---
+++ assets/js/modules/plotly-influence.js
@@ -0,0 +1,95 @@
+/*
+ * Influence
+ * - Who wrote emails most to mailing lists associated with this WG?
+ * - Ordered by year and affiliation (alphabetical)
+ */
+
+function loadInfluenceData(WG, redraw=false, is_small_screen=false) {
+    // Data CSV
+    var file = "/assets/data/dashboard/influence/"+WG+".csv";
+    // FIXME: overview page
+    //var file = "/assets/data/overview/influence/"+WG+".csv";
+
+    // Config for dashboard → influence plot
+    const plotlyConfigDI = {
+        displaylogo: false,
+        responsive: true,
+        toImageButtonOptions: {
+            format: "svg",
+            filename: "almanac.article19.org-influence-"+WG,
+            scale: 1
+        },
+        modeBarButtonsToRemove: ["zoom2d", "pan2d","select2d","lasso2d", "resetScale2d"]
+    };
+
+    d3.csv(file, function(error, rows){
+        // if the file cannot be loaded
+        if (error) {
+            throw error;
+        }
+
+        //console.log("Rows", rows);
+
+        var headerNames = d3.keys(rows[0]);
+        // remove Date as a header
+        headerNames.shift();
+        // case insensitive sorting
+        headerNames.sort((a, b) => a.localeCompare(b, undefined, {sensitivity: "base"}));
+
+        var traces = [];
+        var hexcolor;
+        headerNames.forEach((affiliation) => traces.push({
+                type: "scatter",
+                mode: "none",
+                fill: "tozeroy",
+                name: affiliation,
+                x: unpack(rows, "Date"),
+                y: unpack(rows, affiliation),
+                hoveron: "points+fills",
+                fillcolor: getColorCode(affiliation)
+            })
+        );
+        //console.log(traces);
+
+        var data = traces;
+        var layout = {
+            title: {
+                text: "Who sent most emails to "+WG+"?",
+                xref: "container",
+                x: 0
+            },
+            margin: {
+                t: 35, r: 40, b: 45, l: 40
+            },
+            xaxis: {
+                title: "per year"
+            },
+            yaxis: {
+                title: "Emails sent"
+            },
+            font: {
+                family: "Roboto, sans-serif",
+                size: 12,
+                color: "#101820"
+            },
+            legend: {
+                orientation: "v"
+            }
+        };
+        
+        if(is_small_screen) {
+            layout.legend.orientation = "h";
+            layout.legend.y = -0.75;
+            layout.margin.r = 5;
+        }
+
+        // executed only once, bc this is a module
+        Plotly.newPlot("plotlyDInfluence", data, layout, plotlyConfigDI);
+
+        if(redraw === true) {
+            Plotly.redraw("plotlyDInfluence", data);
+        }
+    })
+}
+
+export { loadInfluenceData }
---
+++ assets/js/modules/plotly-leadership.js
@@ -0,0 +1,93 @@
+
+function loadLeadershipData(WG, redraw=false, is_small_screen=false) {
+    // Load CSV
+    var file = "/assets/data/dashboard/leadership/"+WG+".csv";
+    // FIXME: Load Overview
+    // var file = "/assets/data/overview/leadership/"+WG+".csv";
+
+    // Config for dashboard → leadership plot
+    const plotlyConfigDL = {
+        displaylogo: false,
+        responsive: true,
+        toImageButtonOptions: {
+            format: "svg",
+            filename: "almanac.article19.org-leadership-"+WG,
+            scale: 1
+        },
+        modeBarButtonsToRemove: ["zoom2d", "pan2d","select2d","lasso2d", "resetScale2d"]
+    };
+
+    d3.csv(file, function(error, rows) {
+        if (error) {
+            throw error;
+        }
+
+        rows.sort(orderByAffiliation);
+        //console.log("Rows", rows);
+
+        var traces = [];
+        var affiliation, hexcolor, person, time_start, time_end, wgname;
+
+        for(var i = 0; i < rows.length; i++) {
+            affiliation = rows[i]["affiliation"];
+            person = rows[i]["name"];
+            time_start = rows[i]["time_start"];
+            time_end = rows[i]["time_end"];
+            wgname = (rows[i]["wg"] !== undefined) ? rows[i]["wg"] : WG;
+            hexcolor = getColorCode(affiliation);
+
+            if(time_start != time_end) { // for esthetical reasons: but there's a bug somewhere in the data if that happens
+                traces.push({
+                    type: "scatter",
+                    mode: "lines",
+                    line: {
+                        color: hexcolor,
+                        width: 25
+                    },
+                    name: affiliation,
+                    text: wgname + "/" + person,
+                    x: [ time_start, time_end ],
+                    y: [ affiliation, affiliation ],
+                });
+            }
+        }
+        //console.log("traces", traces);
+
+        var data = traces;
+
+        var layout = {
+            scattermode: "group",
+            title: {
+                text: "Chairing periods: "+WG,
+                xref: "container",
+                x: 0
+            },
+            margin: {
+                t: 35, r: 0, b: 50, l: 40
+            },
+            xaxis: {
+                title: "Chairing period",
+            },
+            yaxis: {
+                automargin: true,
+                autorange: "reversed",
+                showgrid: false,
+            },
+            font: {
+                family: "Roboto, sans-serif",
+                size: 12,
+                color: "#101820"
+            },
+            showlegend: false,
+        };
+
+        // executed only once, bc this is a module
+        Plotly.newPlot("plotlyDLeadership", data, layout, plotlyConfigDL);
+
+        if(redraw === true) {
+            Plotly.redraw("plotlyDLeadership", data);
+        }
+    })
+}
+
+export { loadLeadershipData }
