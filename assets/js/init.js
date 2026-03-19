ece3d0c796b63d0dbc37a74813b64be3dd03fb99
Ulrike Uhlig <u@451f.org>
2026-03-19 22:53:09 +0100

    CSS only progress bar

--- assets/js/init.js
+++ assets/js/init.js
@@ -131,27 +131,3 @@ $(function($){
     });
 
 });
-
-// Progress bar
-$(document).ready(function() {
-    const win = $(window);
-    const doc = $(document);
-    const progressBar = $('progress');
-    const progressLabel = $('.progress-label');
-    const setValue = () => win.scrollTop();
-    const setMax = () => doc.height() - win.height();
-    const setPercent = () => Math.round(win.scrollTop() / (doc.height() - win.height()) * 100);
-
-    progressLabel.text(setPercent() + '%');
-    progressBar.attr({ value: setValue(), max: setMax() });
-
-    doc.on('scroll', () => {
-        progressLabel.text(setPercent() + '%');
-        progressBar.attr({ value: setValue() });
-    });
-
-    win.on('resize', () => {
-        progressLabel.text(setPercent() + '%');
-        progressBar.attr({ value: setValue(), max: setMax() });
-    })
-});
