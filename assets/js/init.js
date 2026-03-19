b7dd13eee5f7900698acf1554b5f46b78277e42c
Ulrike Uhlig <u@451f.org>
2026-03-19 14:46:17 +0100

    progress bar

--- assets/js/init.js
+++ assets/js/init.js
@@ -93,6 +93,7 @@ $(function($){
             $(this).text("Hide…");
         }
     });
+
     /*
     // Generate toc
     $('.guide h2.title').each(
@@ -128,4 +129,29 @@ $(function($){
             }
         }
     });
+
+});
+
+// Progress bar
+$(document).ready(function() {
+    const win = $(window);
+    const doc = $(document);
+    const progressBar = $('progress');
+    const progressLabel = $('.progress-label');
+    const setValue = () => win.scrollTop();
+    const setMax = () => doc.height() - win.height();
+    const setPercent = () => Math.round(win.scrollTop() / (doc.height() - win.height()) * 100);
+
+    progressLabel.text(setPercent() + '%');
+    progressBar.attr({ value: setValue(), max: setMax() });
+
+    doc.on('scroll', () => {
+        progressLabel.text(setPercent() + '%');
+        progressBar.attr({ value: setValue() });
+    });
+
+    win.on('resize', () => {
+        progressLabel.text(setPercent() + '%');
+        progressBar.attr({ value: setValue(), max: setMax() });
+    })
 });
